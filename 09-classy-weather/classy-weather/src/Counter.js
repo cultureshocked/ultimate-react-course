1640365697e28a446b6bab4c16e62a85f4d7970a
Conrad Fitzgerald <cfitz012@uottawa.ca>
2023-07-07 07:57:59 -0400

    Finish classy-weather application using classes.

---
+++ 09-classy-weather/classy-weather/src/Counter.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleDecrement() {
+    this.setState((curState) => {
+      return { ...curState, count: curState.count - 1 };
+    });
+  }
+  handleIncrement() {
+    this.setState((curState) => {
+      return { ...curState, count: curState.count + 1 };
+    });
+  }
+  render() {
+    const date = new Date("june 26 1999");
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()}[{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
